38053dd762ec6d860e1065e14e40158bb21dd143
AislingHeanue <aislingheanue@gmail.com>
2023-10-30 21:12:23 +0000

    GOL: Added support for files, pausing, drawing

--- Game of Life/Go/Game of Life.go
+++ Game of Life/Go/Game of Life.go
@@ -5,16 +5,23 @@ import (
 	"math"
 	"math/rand"
 	"os"
+	"strings"
 
 	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/inpututil"
 )
 
 type LifeGame struct {
-	board        *Board
-	time         int
-	colourPeriod int
-	trailLength  int
-	pixelBuffer  []byte
+	board         *Board
+	visibleX      int
+	visibleY      int
+	time          int
+	colourPeriod  int
+	trailLength   int
+	pixelBuffer   []byte
+	wallThickness int
+	paused        bool
+	speed         float64
 }
 
 type Board struct {
@@ -32,26 +39,40 @@ type Box struct {
 }
 
 func main() {
-	boardX := 500
-	boardY := 500
-	boxSize := 2
+	// TODO: Add a CLI for all of these values
+	boardX := 300
+	boardY := 300
+	scale := 3
 	colourPeriod := 30
-	trailLength := 60
-	x := boardX * boxSize
-	y := boardY * boxSize
+	trailLength := 10
+	wallThickness := 200 // the very edges of the board are dead, but you can specify a buffer zone between the visible area and the boundary
+	speed := 1
+	random := false
+	filepath := "../Life Files/3enginecordershipgun.cells.txt"
 
-	lg, err := initLifeGame(boardX, boardY, colourPeriod, trailLength)
+	paused := (filepath == "" && !random) // start in "editor mode" if not randomised and not loading a file
+	lg, err := initLifeGame(boardX, boardY, colourPeriod, trailLength, wallThickness, paused, float64(speed))
 	if err != nil {
 		fmt.Printf("Error in init: %v", err)
 		os.Exit(1)
 	}
-	random := true // TODO: allow user to draw image, allow loading of life files
-	if random {
+
+	if filepath != "" {
+		err := lg.loadFile(filepath)
+		if err != nil {
+			fmt.Printf("Error in file loading: %v", err)
+			os.Exit(1)
+		}
+	} else if random {
 		lg.randomise()
 	}
 
+	x := int(float64(boardX) * float64(scale))
+	y := int(float64(boardY) * float64(scale))
 	ebiten.SetWindowSize(x, y)
 	ebiten.SetWindowTitle("Game of Life")
+	ebiten.SetWindowResizable(true)
+	ebiten.SetMaxTPS(int(60 * lg.speed))
 	err = ebiten.RunGame(lg)
 	if err != nil {
 		fmt.Printf("Error in game: %v", err)
@@ -72,27 +93,45 @@ func colour(time int, period int, age int, trailLength int) (byte, byte, byte) {
 		))
 }
 
-func initLifeGame(x int, y int, colourPeriod int, trailLength int) (*LifeGame, error) {
+func initLifeGame(x, y, colourPeriod, trailLength, wallThickness int, paused bool, speed float64) (*LifeGame, error) {
 	lg := LifeGame{
-		board:        newBoard(x+2, y+2),
-		pixelBuffer:  make([]byte, x*y*4),
-		colourPeriod: colourPeriod,
-		trailLength:  trailLength,
+		board:         newBoard(x+2*wallThickness, y+2*wallThickness, wallThickness),
+		visibleX:      x,
+		visibleY:      y,
+		pixelBuffer:   make([]byte, x*y*4),
+		colourPeriod:  colourPeriod,
+		trailLength:   trailLength,
+		wallThickness: wallThickness,
+		paused:        paused,
+		speed:         speed,
 	}
 
 	return &lg, nil
 }
 
 func (lg *LifeGame) Update(screen *ebiten.Image) error {
-	lg.board.gameTick()
-	lg.time++
+	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+		lg.paused = !lg.paused
+	}
+	if !lg.paused {
+		lg.board.gameTick()
+		lg.time++
+	} else {
+		if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+			x, y := ebiten.CursorPosition()
+			lg.board.get(x+lg.wallThickness, y+lg.wallThickness).on = true
+		}
+		if ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
+			x, y := ebiten.CursorPosition()
+			lg.board.get(x+lg.wallThickness, y+lg.wallThickness).on = false
+		}
+	}
 
 	return nil
 }
 
 func (lg *LifeGame) Draw(screen *ebiten.Image) {
 	lg.board.Draw(lg.pixelBuffer, lg.time, lg.colourPeriod, lg.trailLength)
-
 	err := screen.ReplacePixels(lg.pixelBuffer)
 	if err != nil {
 		panic(err)
@@ -100,18 +139,66 @@ func (lg *LifeGame) Draw(screen *ebiten.Image) {
 }
 
 func (lg *LifeGame) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return lg.board.sizeX - 2, lg.board.sizeY - 2
+	return lg.visibleX, lg.visibleY
 }
 
 func (lg *LifeGame) randomise() {
-	for x := 1; x < lg.board.sizeX-1; x++ { //ignore walls, they are always dead (TODO: maybe make a larger wall size eg. 20 to simulate an endless board?)
-		for y := 1; y < lg.board.sizeY-1; y++ {
+	for x := lg.wallThickness; x < lg.board.sizeX-lg.wallThickness-1; x++ { //ignore walls, they are always dead
+		for y := lg.wallThickness; y < lg.board.sizeY-lg.wallThickness-1; y++ {
 			lg.board.get(x, y).on = (rand.Int()%2 == 0)
 		}
 	}
 }
 
-func newBoard(sizeX, sizeY int) *Board {
+func (lg *LifeGame) loadFile(filePath string) error {
+	fileBytes, err := os.ReadFile(filePath)
+	if err != nil {
+		return fmt.Errorf("error opening file: %w", err)
+	}
+	fileLines := strings.Split(string(fileBytes), "\n")
+	var description string
+	var fileX, fileY int
+	// extract description, and count the number of rows and columns
+	for _, line := range fileLines {
+		if len(line) == 0 || string(line[0]) == "!" {
+			if len(line) != 0 {
+				description += line + "\n"
+			}
+		} else {
+			fileY += 1
+			if len(line) > fileX {
+				fileX = len(line)
+			}
+		}
+	}
+	if fileX > lg.visibleX {
+		return fmt.Errorf("please specify a board width greater than %d", fileX)
+	}
+	if fileY > lg.visibleX {
+		return fmt.Errorf("please specify a board height greater than %d", fileY)
+	}
+	fmt.Println(description)
+	startX := lg.board.sizeX/2 - fileX/2
+	startY := lg.board.sizeY/2 - fileY/2
+	x := startX
+	y := startY
+	for _, line := range fileLines {
+		if len(line) == 0 || string(line[0]) != "!" {
+			for _, letter := range line {
+				if string(letter) == "O" {
+					lg.board.get(x, y).on = true
+				}
+				x += 1
+			}
+			x = startX
+			y += 1
+		}
+	}
+
+	return nil
+}
+
+func newBoard(sizeX, sizeY, wallThickness int) *Board {
 	b := Board{
 		sizeX: sizeX,
 		sizeY: sizeY,
@@ -129,7 +216,7 @@ func newBoard(sizeX, sizeY int) *Board {
 		b.boxes[i] = Box{
 			on:         false,
 			timeDead:   100,
-			isBoundary: (x == 0 || x == sizeX-1 || y == 0 || y == sizeY-1),
+			isBoundary: (x < wallThickness || x > sizeX-wallThickness-1 || y < wallThickness || y > sizeY-wallThickness-1),
 		}
 	}
 	return &b
@@ -173,7 +260,7 @@ func (b *Board) gameTick() {
 	for i := range b.neighbours {
 		b.neighbours[i] = 0
 	}
-	for x := 1; x < b.sizeX-1; x++ { //ignore walls, they are always dead (TODO: maybe make a larger wall size eg. 20 to simulate an endless board?)
+	for x := 1; x < b.sizeX-1; x++ { //ignore walls, they are always dead
 		for y := 1; y < b.sizeY-1; y++ {
 			if b.get(x, y).on {
 				for _, offset := range b.neighbourList {
@@ -183,7 +270,7 @@ func (b *Board) gameTick() {
 		}
 	}
 	// read neighbours, update board state
-	for x := 1; x < b.sizeX-1; x++ { //ignore walls, they are always dead (TODO: maybe make a larger wall size eg. 20 to simulate an endless board?)
+	for x := 1; x < b.sizeX-1; x++ { //ignore walls, they are always dead
 		for y := 1; y < b.sizeY-1; y++ {
 			if b.neighbours[x+b.sizeX*y] == 3 {
 				b.get(x, y).on = true
